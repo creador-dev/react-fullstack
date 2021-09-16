5377afff89035ea107babe47d87db7be077f6d6f
Pawan Kumar <pwnkumar201@gmail.com>
2021-09-16 18:13:53 +0530

    React State

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
 import styles from './App.module.css';
 
 function App() {
+  // use state for input and todos
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  // add new input ot the existing list
+  const addTodo = (e) => {
+    e.preventDefault()
+    setTodos([...todos, input])
+    setInput('')
+  }
+
   return (
     <div className={styles.app}>
-      <header className={styles.app__header}>
-        <img src={logo} className={styles.app__logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={styles.app__link}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to my todo list</h1>
+      <form onSubmit={addTodo}>
+        <input type="text" value={input} onChange={e => setInput(e.target.value)} required/>
+        <button type="submit">Add todo</button>
+      </form>
+      <h2>List of todos</h2>
+      {todos.length ? 
+        <ul>
+          { todos.map(data => (
+            <li>{data}</li>
+          ))}
+        </ul>
+      : ''}
     </div>
   );
 }
