19d7daeb29042405804d5aa75be238558d7ea6e3
Pawan Kumar <pwnkumar201@gmail.com>
2021-09-16 16:53:17 +0530

    Learned CSS modules

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.app}>
+      <header className={styles.app__header}>
+        <img src={logo} className={styles.app__logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={styles.app__link}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
